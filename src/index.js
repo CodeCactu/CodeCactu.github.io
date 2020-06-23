16c7b41666132881e544306f0cb0b6d424f0c5c2
Evolveye <stolarski.paw@gmail.com>
2020-06-23 18:39:45 +0200

    Reinventing the wheel again

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Background from "./Background.js"
+
+import "./index.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Background />
+  </React.StrictMode>,
+  document.getElementById( `content` )
+)
