6469bf65588dfae361a043f68f73e28f2a18689c
Evolveye <stolarski.paw@gmail.com>
2020-06-26 11:56:56 +0200

    Logo

--- src/Background.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default class Background extends React.Component {
-  render = () => <canvas />
-}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import Background from "./Background.js"
+import Banner from "./components/Banner.js"
+// import CellBackground from "./components/CellBackground.js"
+// import Background from "./components/Background.js"
 
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Background />
+    <Banner />
   </React.StrictMode>,
   document.getElementById( `content` )
 )
