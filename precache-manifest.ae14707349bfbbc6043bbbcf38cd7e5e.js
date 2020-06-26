ca29e81bd271f616f75568ce8380a793bd0c8b92
Evolveye <stolarski.paw@gmail.com>
2020-06-26 14:47:16 +0200

    Updates

---
+++ precache-manifest.ae14707349bfbbc6043bbbcf38cd7e5e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0749fc55d39e974f1b66daa0391594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb9c6966c61e7fb0f29a",
+    "url": "/static/css/main.7bd7beff.chunk.css"
+  },
+  {
+    "revision": "919cfe7bef16f23f4c74",
+    "url": "/static/js/2.ada429a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ada429a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9c6966c61e7fb0f29a",
+    "url": "/static/js/main.4161a9a4.chunk.js"
+  },
+  {
+    "revision": "487dfd73e1afb15c9a0d",
+    "url": "/static/js/runtime-main.1d9fcc38.js"
+  },
+  {
+    "revision": "16249f437375ee67e9f8cb5db91c5be2",
+    "url": "/static/media/Banner-floating_face.16249f43.svg"
+  },
+  {
+    "revision": "f9b9f87e2b3b79bcbb8639af07fe4af2",
+    "url": "/static/media/Banner-grass.f9b9f87e.svg"
+  },
+  {
+    "revision": "ecd2af0cffb679e3cd5fd82a77d1a7d9",
+    "url": "/static/media/Banner-logo.ecd2af0c.svg"
+  },
+  {
+    "revision": "f317e61fac54ffbda1719121031acd87",
+    "url": "/static/media/Banner-no_sign.f317e61f.svg"
+  },
+  {
+    "revision": "1ce5a5c34ea04754fd522f119e4cd1f7",
+    "url": "/static/media/Coconut.1ce5a5c3.ttf"
+  }
+]);
