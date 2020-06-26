b510edf23ec8d1998b1beaaac6c84a747206a3f9
Evolveye <stolarski.paw@gmail.com>
2020-06-26 13:44:31 +0200

    Lines on background

--- src/components/DynamicBackground.js
+++ src/components/DynamicBackground.js
@@ -32,8 +32,56 @@ export class DynamicBackground extends React.Component {
 }
 
 export default class BackgroundLines extends DynamicBackground {
-  state = {}
+  static Line = class Line {
+    constructor( x, y, width, length ) {
+      this.x = x
+      this.y = y
+      this.width = width
+      this.length = length
+    }
+  }
+
+  /** @type {BackgroundLines.Line[]} */
+  lines = []
 
   generateObjects() {
+    const { lines } = this
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+    const random = (min, max) =>
+      Math.floor( Math.random() * (max - min + 1) ) + min
+    const distance = ({ x:aX, y:aY }, { x:bX, y:bY }) =>
+      Math.sqrt( (aX - bX) ** 2 + (aY - bY) ** 2 )
+
+    lines.splice( 0 )
+
+    for (let fails = 0; fails < 100;) {
+      const x = random( 0, windowWidth )
+      const y = random( 0, windowHeight )
+      const width = random( 1, 3 )
+      const length = random( 10, 50 )
+
+      const line = new BackgroundLines.Line( x, y, width, length )
+      let addIt = true
+
+      for (const { x, y } of lines) if (distance( line, { x, y } ) < 100) {
+        addIt = false
+        break
+      }
+
+      if (addIt) lines.push( line )
+      else fails++
+    }
+  }
+
+  animate = () => {
+    const { ctx, lines } = this
+
+    ctx.fillStyle = `red`
+
+    lines.forEach( ({ x, y, width, length }) => {
+      ctx.beginPath()
+      ctx.fillRect( x, y - width / 2, length, width )
+    } )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom"
 
 import Banner from "./components/Banner.js"
 // import CellBackground from "./components/CellBackground.js"
-// import Background from "./components/Background.js"
+import Background from "./components/DynamicBackground.js"
 
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <Background />
     <Banner />
   </React.StrictMode>,
   document.getElementById( `content` )
