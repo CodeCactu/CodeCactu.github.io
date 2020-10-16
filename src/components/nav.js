cc1f1064bade426faa87324cf23956cc495e35c3
Evolveye <stolarski.paw@gmail.com>
2020-10-17 00:59:45 +0200

    Nav styles

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import styles from "./nav.module.css"
+
+const items = [
+  `GitHub`,
+  `Discord bot framework`,
+  `CactuJams`,
+]
 
 export default () =>
-  <> </>
+  <nav className={styles.nav}>
+    <ul className={styles.items}>
+      {
+        items.map( item =>
+          <li className={styles.item}>
+            <Link to="" className={`${styles.link} is-not-decorative`}>
+              <span className={`${styles.bracket} ${styles.isLeft}`}>[</span>{item}<span className={`${styles.bracket} ${styles.isRight}`}>]</span>
+            </Link>
+          </li>
+        )
+      }
+    </ul>
+  </nav>
