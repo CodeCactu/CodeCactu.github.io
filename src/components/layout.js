3bcdfdf528f9c66ac279bd222aab4abbd836941e
Evolveye <stolarski.paw@gmail.com>
2020-10-17 00:19:40 +0200

    z-index of layout elements fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,13 @@ import React from "react"
 import Background from "./DynamicBackground"
 import Nav from "./nav"
 
+import "./sanitize.css"
 import styles from "./layout.module.css"
 
 export default ({ children }) =>
   <div className={styles.layout}>
-    <Nav />
     <Background />
+    <Nav />
     <main className={styles.main}>
       {children}
     </main>
