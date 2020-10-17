31b641af01d948e1ebe700d75718182669760872
Evolveye <stolarski.paw@gmail.com>
2020-10-17 02:02:32 +0200

    Links with empty address maker

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,19 +4,19 @@ import { Link } from "gatsby"
 import styles from "./nav.module.css"
 
 const items = [
-  `GitHub`,
-  `Discord bot framework`,
-  `CactuJams`,
+  { name:`GitHub`, address:`` },
+  { name:`Discord bot framework`, address:`` },
+  { name:`CactuJams`, address:`` },
 ]
 
 export default () =>
   <nav className={styles.nav}>
     <ul className={styles.items}>
       {
-        items.map( item =>
-          <li className={styles.item}>
-            <Link to="" className={`${styles.link} is-not-decorative`}>
-              <span className={`${styles.bracket} ${styles.isLeft}`}>[</span>{item}<span className={`${styles.bracket} ${styles.isRight}`}>]</span>
+        items.map( ({ name, address }) =>
+          <li key={name} className={styles.item}>
+            <Link to={address} className={`${styles.link} is-not-decorative`}>
+              <span className={`${styles.bracket} ${styles.isLeft}`}>[</span>{name}<span className={`${styles.bracket} ${styles.isRight}`}>]</span>
             </Link>
           </li>
         )
