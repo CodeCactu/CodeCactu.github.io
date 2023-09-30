86c7eabc2b976fd217103a4f1c6aa607e74d1891
evolveye <stolarski.paw@gmail.com>
2023-10-01 01:39:22 +0200

    Glowing tiles effect of mouse moving

--- src/features/dynamicBackgrounds/CactuGradients/logic.ts
+++ src/features/dynamicBackgrounds/CactuGradients/logic.ts
@@ -1,6 +1,13 @@
 import { UiManager, UiManagerHolder } from "@lib/dynamicUi"
 
-let i = 0
+type Point = {
+  x: number
+  y: number
+}
+
+type MouseHoverTile = Point & {
+  createdAt: number
+}
 
 export default class CactuGradientsController implements UiManagerHolder {
   uiManager: UiManager<HTMLDivElement>
@@ -9,31 +16,62 @@ export default class CactuGradientsController implements UiManagerHolder {
     test: true,
   }
 
+  tileSize = 10
+  mouseTileHistory: MouseHoverTile[] = []
+
 
   constructor( root:HTMLDivElement ) {
     this.uiManager = new UiManager( root )
     this.ctx = this.uiManager.registerCtx( `main`, `canvas` )
-
-    this.uiManager.startLoop( () => {
-      this.draw()
-    } )
+    this.uiManager.registerEvent( this.uiManager.rootElement, `mousemove`, e => this.handleMouseMove( e ) )
+    this.uiManager.startLoop( () => this.update() )
   }
 
-  logic() {
-    // Enjoy the silence
-  }
-
-  draw() {
-    const { ctx } = this
-
-    ctx.strokeStyle = `red`
+  update() {
+    const { ctx, tileSize } = this
+    const padding = 2
 
     ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height )
 
-    ctx.beginPath()
-    ctx.arc( i % ctx.canvas.width, ctx.canvas.height / 2, 5, 0, Math.PI * 2 )
-    ctx.stroke()
+    ctx.shadowBlur = tileSize * 2
+    ctx.shadowColor = `rgb( 128, 0, 255 )`
 
-    i++
+    for (let i = 0;  i < this.mouseTileHistory.length;  ++i) {
+      const { x, y, createdAt } = this.mouseTileHistory[ i ]
+      const seconds = Math.floor( (Date.now() - createdAt ) )
+
+      if (seconds > 1000) {
+        this.mouseTileHistory.splice( i )
+        break
+      }
+
+      ctx.beginPath()
+      ctx.fillStyle = `rgba( 128, 0, 255, ${1 - seconds / 1000} )`
+      ctx.arc( x * tileSize + tileSize / 2, y * tileSize + tileSize / 2, tileSize, 0, Math.PI * 2 )
+      ctx.fill()
+    }
+
+    ctx.shadowBlur = 0
+    ctx.fillStyle = `#fff`
+
+    for (const { x, y } of this.mouseTileHistory) {
+      ctx.fillRect( x * tileSize + padding, y * tileSize + padding, tileSize - padding, tileSize - padding )
+    }
+  }
+
+  getTileByabsoluteCoords( x:number, y:number ) {
+    return {
+      x: Math.floor( x / this.tileSize ),
+      y: Math.floor( y / this.tileSize ),
+    }
+  }
+
+  handleMouseMove( e:MouseEvent ) {
+    const mouseTile = this.getTileByabsoluteCoords( e.clientX, e.clientY )
+    const newestTile = this.mouseTileHistory[ 0 ]
+
+    if (newestTile && newestTile.x === mouseTile.x && newestTile.y === mouseTile.y) return
+
+    this.mouseTileHistory.unshift({ ...mouseTile, createdAt:Date.now() })
   }
 }
