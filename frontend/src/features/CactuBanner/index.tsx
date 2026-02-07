9c9b4a5e5c96df6c686d04cee1cd5b4227d8a358
evolveye <stolarski.paw@gmail.com>
2026-02-07 22:03:31 +0100

    RWD

--- frontend/src/features/CactuBanner/index.tsx
+++ frontend/src/features/CactuBanner/index.tsx
@@ -1,5 +1,5 @@
+import Image from "next/image"
 import cn from "@lib/core/functions/createClassName"
-import Image from "@lib/core/flow/Image"
 import noSign from "./svg/Banner-no_sign.svg"
 import logo from "./svg/Banner-logo.svg"
 import grass from "./svg/Banner-grass.svg"
@@ -15,16 +15,16 @@ export default function CactuBanner({ className }:CactuBannerProps) {
     <section className={cn( classes.banner, className )}>
       <Image className={cn( classes.signet )} src={logo.src} width={294} height={345} alt="Cactu signet" priority />
 
-      <Image className={cn( classes.singleSign, classes.floatingFace, classes.is1 )} src={floating_face.src} alt="Floating face" />
-      <Image className={cn( classes.singleSign, classes.floatingFace, classes.is2 )} src={floating_face.src} alt="Floating face" />
+      <Image className={cn( classes.singleSign, classes.floatingFace, classes.is1 )} src={floating_face.src} width={168} height={137} alt="Floating face" />
+      <Image className={cn( classes.singleSign, classes.floatingFace, classes.is2 )} src={floating_face.src} width={168} height={137} alt="Floating face" />
 
-      <Image className={cn( classes.singleSign, classes.grass, classes.is1 )} src={grass.src} alt="Grass" />
-      <Image className={cn( classes.singleSign, classes.grass, classes.is2 )} src={grass.src} alt="Grass" />
-      <Image className={cn( classes.singleSign, classes.grass, classes.is3 )} src={grass.src} alt="Grass" />
+      <Image className={cn( classes.singleSign, classes.grass, classes.is1 )} src={grass.src} width={100} height={45} alt="Grass" />
+      <Image className={cn( classes.singleSign, classes.grass, classes.is2 )} src={grass.src} width={100} height={45} alt="Grass" />
+      <Image className={cn( classes.singleSign, classes.grass, classes.is3 )} src={grass.src} width={100} height={45} alt="Grass" />
 
       <p className={classes.logotype}>cactu</p>
 
-      <Image className={cn( classes.singleSign, classes.noSign )} src={noSign.src} alt="No sign" />
+      <Image className={cn( classes.singleSign, classes.noSign )} src={noSign.src} width={100} height={66} alt="No sign" />
     </section>
   )
 }
